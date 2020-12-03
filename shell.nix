1d3c88ac3959e05bce48246d3ff6920153b143be
Martin Toman <toman.martin@live.com>
2020-12-03 18:19:52 +0100

    Add rstudio

--- shell.nix
+++ shell.nix
@@ -2,8 +2,25 @@
 
 with pkgs;
 
+let
+  my-rstudio = rstudioWrapper.override { packages = with rPackages; [
+    curl
+
+    ggplot2
+    astsa
+    tseries
+    forecast
+    fGarch
+
+    knitr
+    rmarkdown
+  ]; };
+in
 mkShell {
   buildInputs = [
+    my-rstudio
+    pandoc
+    texlive.combined.scheme-full
   ];
   shellHook = ''
   '';
